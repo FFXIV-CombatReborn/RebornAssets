d4bd27fa386a63f708e7df36e59296fbe3b79df9
Ehl'tee Es <127939494+LTS-FFXIV@users.noreply.github.com>
2024-05-02 02:37:08 -0500

    Create iconIDreference.cs

---
+++ iconIDreference.cs
@@ -0,0 +1,9 @@
+  23// AhkRhai, spread marker
+  62// stacks
+  70
+  161// stackmarker
+  197// Nox, chasing AOE marker
+  218// tankbuster
+  267// AccelerationBomb
+  316// Styx, stack, multihit
+  326// chain
